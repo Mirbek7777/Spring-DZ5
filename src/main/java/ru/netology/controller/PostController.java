3c73b1c4c17e93dc763adb0197b0038e4d018f76
Mirbek <mirbek_karimberdiev@mail.ru>
2023-07-11 10:05:28 +0300

    1th commit

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,52 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
+import ru.netology.dto.PostDto;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+  private final PostService service;
+
+  public PostController(PostService service) {
+    this.service = service;
+  }
+
+  @GetMapping
+  public List<PostDto> all() {
+    return service.all();
+  }
+
+  @GetMapping("/{id}")
+  public PostDto getById(@PathVariable long id) {
+    return service.getById(id);
+  }
+
+  @PostMapping
+  public PostDto save(@RequestBody Post post) {
+    return service.save(post);
+  }
+
+  @DeleteMapping("/{id}")
+  public void removeById(@PathVariable long id) {
+    service.removeById(id);
+  }
+
+  @GetMapping("/all")
+  public List<Post> allPosts() {
+    return service.allPosts();
+  }
+}
+  @DeleteMapping("/{id}")
+  public void removeById(long id, HttpServletResponse response) {
+    // TODO: deserialize request & serialize response
+  }
+}
---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.dto.PostDto;
+import ru.netology.mapping.MappingUtils;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+
+
+@Service
+public class PostService {
+  private final PostRepository repository;
+  private final MappingUtils mapping;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+    mapping = new MappingUtils();
+  }
+
+  public List<PostDto> all() {
+    return mapping.findAllDto(repository);
+  }
+
+  public PostDto getById(long id) {
+    return mapping.getPostDtoById(repository,id);
+  }
+
+  public PostDto save(Post post) {
+    return mapping.savePost(repository,post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+
+  public List<Post> allPosts(){
+    return repository.all();
+  }
+}
+
